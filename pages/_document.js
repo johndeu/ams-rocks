1dbb6e51d86d665adf5ddcee78b31721d6538597
John Deutscher <johndeu@microsoft.com>
2021-07-16 14:08:42 -0700

    updates

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#000000" />
-          <link rel="shortcut icon" href="/img/favicon.png" />
+          <link rel="shortcut icon" href="/img/ams.svg" />
           <link
             rel="apple-touch-icon"
             sizes="76x76"
