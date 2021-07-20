5686a6ada85e21744c66a7f4775455271f598093
John Deutscher <johndeu@microsoft.com>
2021-07-20 10:41:06 -0700

    fix image

--- components/Blog/coverImage.js
+++ components/Blog/coverImage.js
@@ -1,18 +1,15 @@
 import cn from 'classnames'
 import Link from 'next/link'
-import Image from 'next/image'
 
 export default function CoverImage({ title, src, slug, height, width }) {
   const image = (
-    <Image
+    <img
       src={src}
       alt={`Cover Image for ${title}`}
       className={cn('shadow-sm', {
         'hover:shadow-md transition-shadow duration-200': slug,
       })}
       layout="responsive"
-      width={width}
-      height={height}
     />
   )
   return (
