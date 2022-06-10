eafdacb7a732a4cebffa91a73ca316a6948f034d
John Deutscher <johndeu@microsoft.com>
2022-06-10 13:49:52 -0700

    fixes

--- styles/jss/nextjs-material-kit/pages/landingPageSections/sectionGetStarted.js
+++ styles/jss/nextjs-material-kit/pages/landingPageSections/sectionGetStarted.js
@@ -1,13 +1,20 @@
 import { container } from "styles/jss/nextjs-material-kit.js";
 
-const tabsStyle = {
+const getStartedStyle = (theme) => ({
   section: {
+    [theme.breakpoints.down("sm")]: {
+      textAlign:"center",
+    },
     textAlign:"center",
     color:"#000",
     backgroundColor: "#004578",
     width: "100%",
   },
   ready: {
+    [theme.breakpoints.down("xs")]: {
+      textAlign:"center",
+      padding:"10px 0px"
+    },
     textAlign:"right",
     padding: "20px 5px 20px",
     margin: "0 0",
@@ -16,15 +23,19 @@ const tabsStyle = {
   },
   getStartedContainer: {
     padding: "20px 24px",
+    [theme.breakpoints.down("xs")]: {
+      textAlign:"center",
+      padding:"10px 0px"
+    },
     textAlign:"left",
-
   },
+ 
   getStartedButton: {
     padding: "10px 10px",
     verticalAlign:"bottom",
     color:"#fff",
   },
   container,
-};
+});
 
-export default tabsStyle;
+export default getStartedStyle;
