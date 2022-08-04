024c98e4904cc0ca081a87f3f81c9f05314056c9
John Deutscher <johndeu@microsoft.com>
2022-08-04 13:10:47 -0600

    fixed

--- api/getiptolocation/index.ts
+++ api/getiptolocation/index.ts
@@ -1,27 +1,31 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import fetch from 'node-fetch'
+import axios from "axios";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('Get IP to Location function triggered');
     const ip = (req.query.ip || (req.body && req.body.ip));
+    const subscriptionKey = process.env.MAPS_SUBSCRIPTION_KEY as string;
 
-    fetch("https://atlas.microsoft.com/geolocation/ip/json?api-version=1.0&ip={ip}", {
-        method: "Get",
-    }).then((response) => {
-        console.log(response);
-        JSON.stringify(response.body)
-    }).then( (data) => {
-        console.log(data);
-    }).catch((err) => {
-        console.error(err);
-    })
+    await axios
+        .get(`https://atlas.microsoft.com/geolocation/ip/json?api-version=1.0&ip=${ip}&subscription-key=${subscriptionKey}`)
+        .then(res => {
+            console.log("Status:" + res.status);
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: {
-            countryRegion: "US"
-        }
-    };
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: {
+                    countryRegion: res.data.countryRegion.isoCode
+                }
+            };
+
+        })
+        .catch((err) => {
+            console.error("Error calling Map API geolocation: " + err.message);
+            context.res= {
+                status:500,
+            }
+            return;
+        })
 
 };
 
