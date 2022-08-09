63de12183a5c3c22d02d6a767a028aafbe7e9b4f
John Deutscher <johndeu@microsoft.com>
2022-08-09 11:42:26 -0600

    Updated

--- pages/[lang]/demo.js
+++ pages/[lang]/demo.js
@@ -584,17 +584,19 @@ export default function DemoPage(props) {
         };
     }, [])
 
-    const playBackButton = <Button
-        color="transparent"
-        size="sm"
-        border="1px solid"
-        target="_blank"
-        onClick={() => { navigator.clipboard.writeText(playbackUrl); setCopySuccess('Copied!')}}
-    >
-        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" /><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" /></svg>
-        Copy Playback Url
-        <span className={classes.copySuccess}>{copySuccess}</span>
-    </Button>
+    const playBackButton = <>
+        <Button
+            color="transparent"
+            size="sm"
+            border="1px solid"
+            target="_blank"
+            onClick={() => { navigator.clipboard.writeText(playbackUrl); setCopySuccess('Copied!') }}
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" /><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" /></svg>
+            Copy Playback Url
+
+        </Button> <span className={classes.copySuccess}>{copySuccess}</span>
+    </>
 
     return (
         <div >
