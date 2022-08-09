57cb7eee199e212f0a89039a489628c85a263c06
John Deutscher <johndeu@microsoft.com>
2022-08-09 11:57:35 -0600

    fixes

--- pages/[lang]/demo.js
+++ pages/[lang]/demo.js
@@ -590,12 +590,12 @@ export default function DemoPage(props) {
             size="sm"
             border="1px solid"
             target="_blank"
-            onClick={() => { navigator.clipboard.writeText(playbackUrl); setCopySuccess('Copied!') }}
+            onClick={() => { navigator.clipboard.writeText(playbackUrl); setCopySuccess('Copied!'); setTimeout(() =>{setCopySuccess(null)}, 5000) }}
         >
             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" /><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" /></svg>
             Copy Playback Url
-
-        </Button> <span className={classes.copySuccess}>{copySuccess}</span>
+            <span className={classes.copySuccess}>{copySuccess}</span>
+        </Button> 
     </>
 
     return (
