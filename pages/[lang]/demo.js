ed2f011f62eae76111ddb19f5e51091d95c1a0ff
John Deutscher <johndeu@microsoft.com>
2022-08-09 12:55:25 -0600

    updated

--- pages/[lang]/demo.js
+++ pages/[lang]/demo.js
@@ -107,7 +107,7 @@ export default function DemoPage(props) {
     const [microphones, setAudioInputs] = useState([]);
     const [livePlayback, setLivePlayback] = useState(null);
     const [playbackUrl, setPlaybackUrl] = useState(null);
-    const [copySuccess, setCopySuccess] = useState(false);
+    const [copySuccess, setCopySuccess] = useState(null);
     const [noEvents, setNoEvents] = useState(false);
     const [loadingProgress, setLoadingProgress] = useState(0);
     const [demoState, setDemoState] = useState(STATES.INTRO);
@@ -590,11 +590,11 @@ export default function DemoPage(props) {
             size="sm"
             border="1px solid"
             target="_blank"
-            onClick={() => { navigator.clipboard.writeText(playbackUrl); setCopySuccess('Copied!'); setTimeout(() =>{setCopySuccess(null)}, 5000) }}
+            onClick={() => { navigator.clipboard.writeText(playbackUrl); setCopySuccess(true); setTimeout(() =>{setCopySuccess(false)}, 3000) }}
         >
             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" /><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" /></svg>
             Copy Playback Url
-            <span className={classes.copySuccess}>{copySuccess}</span>
+            <span className={classes.copySuccess}>{copySuccess? 'Copied!': ''}</span>
         </Button> 
     </>
 
