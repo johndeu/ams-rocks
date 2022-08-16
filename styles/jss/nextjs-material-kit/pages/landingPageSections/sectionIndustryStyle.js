aef2b66607cc9af1dacc3a9679fe4e3af3c3f619
John Deutscher <johndeu@microsoft.com>
2022-08-16 10:59:52 -0600

    fixed scroll on industry section

--- styles/jss/nextjs-material-kit/pages/landingPageSections/sectionIndustryStyle.js
+++ styles/jss/nextjs-material-kit/pages/landingPageSections/sectionIndustryStyle.js
@@ -1,12 +1,21 @@
 import { container } from "styles/jss/nextjs-material-kit.js";
 import imagesStyle from "styles/jss/nextjs-material-kit/imagesStyles.js";
 
-const tabsStyle = {
+const tabsStyle = (theme) => ({
   section: {
     padding: "15px 0",
     paddingBottom: "42px",
-    align:"center",
-    textAlign:"left !important"
+    align: "center",
+    textAlign: "left !important",
+    [theme.breakpoints.down("xs")]: {
+      "& div.MuiTabs-flexContainer": {
+        overflowX: "scroll",
+        width: "-webkit-fill-available"
+      },
+      "& div.MuiTabs-root": {
+        width: "-webkit-fill-available"
+      }
+    }
   },
   container,
   textSection: {
@@ -15,9 +24,9 @@ const tabsStyle = {
     textAlign: "left !important",
   },
   ...imagesStyle,
-  imgIndustry : {
-   
+  imgIndustry: {
+
   }
-};
+});
 
 export default tabsStyle;
