30b7607e2f26099db2033009a784a8f5b5c68109
John Deutscher <johndeu@microsoft.com>
2022-08-16 15:32:20 -0600

    edits

--- styles/jss/nextjs-material-kit/components/featuresStyle.js
+++ styles/jss/nextjs-material-kit/components/featuresStyle.js
@@ -2,13 +2,18 @@
 import { container } from "styles/jss/nextjs-material-kit.js";
 
 
-const featuresStyle = {
+const featuresStyle = (theme) => ({
     section: {
         padding: "40px 20px 0",
         align: "center",
         textAlign: "left !important",
         color: "#000 !important",
-        backgroundColor: "#FAF9F8"
+        backgroundColor: "#FAF9F8",
+        [theme.breakpoints.down("xs")]: {
+            "& div.MuiTabs-root": {
+                paddingBottom: "60px"
+            }
+        }
     },
     textSection: {
         padding: "15px",
@@ -17,9 +22,9 @@ const featuresStyle = {
     navBox: {
         borderRadius: "0px",
     },
-    
+
     container
-};
+});
 
 
 
